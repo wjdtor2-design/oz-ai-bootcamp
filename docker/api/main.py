7606a79501389187631e08bc39566b99a0cd295f
곽지은 <wjdtor2@gmail.com>
2026-03-09 19:00:38 +0900

    3/9수업내용

--- docker/api/main.py
+++ docker/api/main.py
@@ -1,18 +1,34 @@
-from fastapi import FastAPI
-from sqlalchemy import text
+import json
+import uuid
 
-from database import SessionFactory
+from redis import asyncio as aredis
+from fastapi import FastAPI, Body
+
+
+redis_client = aredis.from_url("redis://redis:6379", decode_responses=True)
 
 app = FastAPI()
 
+# [1] 클라이언트에서 질문(question)을 요청한다.
+@app.post("/chats")
+async def chat_handler(
+    question: str = Body(..., embed=True),
+):
+    # [2] 결과 채널을 구독
+    job_id = str(uuid.uuid4()) # 작업을 식별할 수 있는 랜덤 식별자 발금
+    channel = f"result:{job_id}"
 
-@app.get("/")
-async def root_handler():
-    return {"ping": {"pong123"}}
+    pubsub = redis_client.pubsub()
+    await pubsub.subscribe(channel)
 
-@app.get("/users")
-async def get_users_handler():
-    with SessionFactory() as session:
-        stmt = text("SELECT * FROM user;")
-        result = session.execute(stmt).mappings().all()
+    # [3] 답변 생성 작업 Enqueue
+    job = {"id": job_id, "question": question}
+    await redis_client.lpush("inference_queue", json.dumps(job))
+
+    # [4] 답변 생성 결과를 돌려받기
+    result = None
+    async for message in pubsub.listen():
+        if message["type"] == "message":
+            result = message["data"]
+            break
     return {"result": result}
--- docker/worker/main.py
+++ docker/worker/main.py
@@ -1,14 +1,51 @@
-## Llama 추론 프로그램
+# 답변 생성기
+import json
 import redis
+from llama_cpp import Llama
 
 
 # redis = 엄청 빠른 초소형 데이터베이스(key:value)
-redis_client= redis.from_url("redis://redis:6379", decode_response=True)
+redis_client= redis.from_url("redis://redis:6379", decode_responses=True)
 
+llm = Llama(
+    model_path="./models/Llama-3.2-1B-Instruct-Q4_K_M.gguf",
+    n_ctx=4096,
+    n_threads=2,
+    verbose=False,
+    chat_format="llama-3"
+)
+
+SYSTEM_PROMPT = (
+    "You are a concise assistant. "
+    "Always reply in the same language as the user's input. "
+    "Do not change the language. "
+    "Do not mix languages."
+)
+
+def create_response(question: str):
+    response = llm.create_chat_completion(
+        messages=[
+            {"role": "system", "content": SYSTEM_PROMPT},
+            {"role": "user", "content": question},
+        ],
+        max_tokens=256,
+        temperature=0.7,
+    )
+    return response["choices"][0]["message"]["content"]
 
 def run():
-    # while True:
-    pass
+    while True:
+        # [1] Job을 deque
+        _, job_data = redis_client.brpop("inference_queue")
+        job: dict = json.loads(job_data)
+
+        # [2] 추론
+        answer = create_response(question=job["question"])
+
+        # [3] 결과를 API 서버로 반환
+        channel = f"result:{job["id"]}"
+        redis_client.publish(channel, answer)
+    
 
 # python main.py를 직접 실행되었을 때만, run()을 호출
 if __name__ == "__main__":
